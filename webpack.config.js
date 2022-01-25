63da39a6538df9ec0be785bdfa10d88af771f63b
tresorsawasawa <tresorsawasawa@gmail.com>
2022-01-25 23:23:12 +0200

    Update dist/files

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'none',
+  mode: 'production',
   entry: './src/index.js',
   devtool: false,
   devServer: {
