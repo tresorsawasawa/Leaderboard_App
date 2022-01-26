4a06692e9b6b54f6cf1a4caa3f64f6176efcb9dd
tresorsawasawa <tresorsawasawa@gmail.com>
2022-01-26 22:05:08 +0200

    Add renderScore functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './scss/style.scss';
+import getResponse, { getScores } from './modules/api.js';
+
+const renderScore = (scores) => {
+  const scoresList = document.querySelector('.scores-list');
+  scoresList.innerHTML = '';
+  scores.forEach(({ user, score }) => {
+    const scoreItem = document.createElement('li');
+    scoreItem.classList.add('score-item');
+    scoreItem.innerHTML = `<span>${user}</span>: <span>${score}</span>`;
+    scoresList.appendChild(scoreItem);
+  });
+};
+
+
