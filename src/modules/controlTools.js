760d288563babe0376e6f97d165412780a1d13bd
tresorsawasawa <tresorsawasawa@gmail.com>
2022-01-27 10:18:06 +0200

    Add refresh data functionality and move renderScore functionslity from index.js

---
+++ src/modules/controlTools.js
@@ -0,0 +1,18 @@
+import { getScores } from './api.js';
+
+export const renderScore = (scores) => {
+  const scoresList = document.querySelector('.scores-list');
+  scoresList.innerHTML = '';
+  scores.forEach(({ user, score }) => {
+    const scoreItem = document.createElement('li');
+    scoreItem.classList.add('score-item');
+    scoreItem.innerHTML = `<span>${user}</span>: <span>${score}</span>`;
+    scoresList.appendChild(scoreItem);
+  });
+};
+
+export const refreshData = async () => {
+  const result = await getScores();
+  const { result: data } = await result.json();
+  renderScore(data);
+};
