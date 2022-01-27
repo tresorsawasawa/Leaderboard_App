796f20162c1046a1b4d33c2b44b2dbd45fd6e7cc
tresorsawasawa <tresorsawasawa@gmail.com>
2022-01-27 10:22:21 +0200

    Move renderScore funtionality to controlTools and use refreshData functionality

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
 import './scss/style.scss';
-import getResponse, { getScores } from './modules/api.js';
-
-const renderScore = (scores) => {
-  const scoresList = document.querySelector('.scores-list');
-  scoresList.innerHTML = '';
-  scores.forEach(({ user, score }) => {
-    const scoreItem = document.createElement('li');
-    scoreItem.classList.add('score-item');
-    scoreItem.innerHTML = `<span>${user}</span>: <span>${score}</span>`;
-    scoresList.appendChild(scoreItem);
-  });
-};
+import getResponse from './modules/api.js';
+import { refreshData } from './modules/controlTools.js';
 
 const form = document.querySelector('#add-score-form');
 const inputName = form.querySelector('.input-name');
@@ -18,9 +8,7 @@ const inputScore = form.querySelector('.input-score');
 const refreshBtn = document.querySelector('.refresh-btn');
 
 refreshBtn.addEventListener('click', async () => {
-  const result = await getScores();
-  const { result: data } = await result.json();
-  renderScore(data);
+  refreshData();
 });
 
 form.addEventListener('submit', async (e) => {
@@ -29,13 +17,11 @@ form.addEventListener('submit', async (e) => {
     user: inputName.value,
     score: inputScore.value,
   };
-  getResponse(newPalyer);
-  form.querySelector('.input-name').value = '';
-  form.querySelector('.input-score').value = '';
+  await getResponse(newPalyer);
+  form.reset();
+  refreshData();
 });
 
 window.onload = async () => {
-  const result = await getScores();
-  const { result: data } = await result.json();
-  renderScore(data);
+  refreshData();
 };
